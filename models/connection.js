2d3d755dd5ba05459e489660a1021dc6383b80fa
Arnaud Hunter <arnaudhunter@hotmail.com>
2021-07-09 10:43:31 +0200

    premier export

---
+++ models/connection.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var options = {
+    connectTimeoutMS: 5000,
+    useUnifiedTopology : true,
+    useNewUrlParser: true,
+}
+
+mongoose.connect('mongodb+srv://dev:capsule@cluster0.xp1w4.mongodb.net/morningnews?retryWrites=true&w=majority',
+    options,
+    function(err){
+        console.log(err);
+    }
+)
+
+module.exports = mongoose
